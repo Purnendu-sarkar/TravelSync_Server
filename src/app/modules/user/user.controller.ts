00889f5656ee719b4ea3dd6b8b6060bd730653ca
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2025-12-03 15:37:23 +0600

    Implement pick helper for extracting query parameters and integrate with getAllUsers API

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 import catchAsync from "../../shared/catchAsync";
 import sendResponse from "../../shared/sendResponse";
 import { UserService } from "./user.service";
+import pick from "../../helper/pick";
 
 const createTraveler = catchAsync(async (req: Request, res: Response) => {
     const result = await UserService.createTraveler(req);
@@ -17,6 +18,8 @@ const createTraveler = catchAsync(async (req: Request, res: Response) => {
 })
 
 const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+    const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"])
+
     const page = Number(req.query.page) || 1;
     const limit = Number(req.query.limit) || 10;
     const searchTerm = req.query.searchTerm as string | undefined;
